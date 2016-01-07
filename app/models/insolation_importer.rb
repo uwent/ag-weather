7d82d7c1a268e292f362c1a82bcf40d273ba6dde
Nathan Werbeckes <werbeckes@gmail.com>
2016-01-07 14:39:17 -0600

    Adds .formatted_date to InsolationImporter

--- app/models/insolation_importer.rb
+++ app/models/insolation_importer.rb
@@ -1,11 +1,16 @@
 class InsolationImporter
 
   def self.fetch_day(date)
-    # http://prodserv1.ssec.wisc.edu/insolation/
+    # http://prodserv1.ssec.wisc.edu/insolation/INSOLEAST/INSOLEAST.#{formatted_date(date)}
+
 
 
 
     InsolationDatum.create
     true
+  end
+
+  def self.formatted_date(date)
+    "#{date.year}#{date.doy.to_s.rjust(3, '0')}"
   end
 end
