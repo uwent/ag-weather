8837ef87efbf4c44380e9dbf975f7181439a913c
Nathan Werbeckes <werbeckes@gmail.com>
2016-01-11 16:06:12 -0600

    Test fixes

--- spec/models/data_import_spec.rb
+++ spec/models/data_import_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe DataImport, type: :model do
 
   describe '.days_to_load_for' do
-    it 'lists the days that have not successfully completed' do
+    it 'lists the days that have not successfully successful' do
       expect(DataImport.days_to_load_for('insolation').first).to be_a(Date)
     end
 
     it 'only lists days that are within the defined range' do
-      earliest_date = Date.today - DataImport::DaysBackWindow
+      earliest_date = Date.today - DataImport::DAYS_BACK_WINDOW
 
       expect(DataImport.days_to_load_for('insolation').min).to be >= earliest_date
     end
 
     context 'no successful loads' do
       it 'returns all dates in window' do
-        expect(DataImport.days_to_load_for('insolation').count).to be DataImport::DaysBackWindow
+        expect(DataImport.days_to_load_for('insolation').count).to be DataImport::DAYS_BACK_WINDOW
       end
     end
 
     context 'one successful load' do
       let!(:succesful_load) { DataImport.create!(
         data_type: 'insolation',
-        status: 'completed',
-        readings_from: Date.today - DataImport::DaysBackWindow.days)}
+        status: 'successful',
+        readings_from: Date.today - DataImport::DAYS_BACK_WINDOW.days)}
 
       it 'returns all other days' do
         dates_to_load = [Date.yesterday, Date.today - 2.days]
