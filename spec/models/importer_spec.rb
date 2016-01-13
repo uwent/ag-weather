4b3fa367b602f675f4e866a995ec58d1245f3aa5
Ryland Herrick <ryalnd@gmail.com>
2016-01-12 18:58:22 -0600

    Better test for whether we have grib tools installed
    grib_ls without any arguments will fail, which leads to a false negative
    here.

--- spec/models/importer_spec.rb
+++ spec/models/importer_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Importer do
   it 'has the grib tools available' do
-    expect(system('grib_ls')).to be(true)
+    expect(system('grib_info')).to be(true)
   end
 end
