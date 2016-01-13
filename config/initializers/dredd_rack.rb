7fd9c0e28eb42fd4df9233d589b800c98845c1d0
Ryland Herrick <ryalnd@gmail.com>
2016-01-12 22:08:15 -0600

    Rescue from LoadError when trying to initialize dredd
    This was blowing up when trying to deploy to a non-development
    environment (heroku).
    
    If we can't load dredd, it means we're not in an
    environment in which we care about dredd, so we can simply rescue the
    LoadError and move along.

--- config/initializers/dredd_rack.rb
+++ config/initializers/dredd_rack.rb
@@ -1,6 +1,9 @@
-require 'dredd/rack'
+begin
+  require 'dredd/rack'
 
-# Allow the automatic setup of a local application server when necessary
-#
-# Find the name of your application in its `config.ru` file.
-Dredd::Rack.app = Rails.application
+  # Allow the automatic setup of a local application server when necessary
+  #
+  # Find the name of your application in its `config.ru` file.
+  Dredd::Rack.app = Rails.application
+rescue LoadError
+end
