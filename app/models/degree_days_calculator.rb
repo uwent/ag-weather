4196f9f27638aa99c54081cae669f02ac7b7c660
Allen Ballman <aballman@gmail.com>
2016-03-07 22:14:55 -0500

    Set database.yml back to default and added tests.

--- app/models/degree_days_calculator.rb
+++ app/models/degree_days_calculator.rb
@@ -1,12 +1,20 @@
-class DegreeDaysCalculator 
+class DegreeDaysCalculator
   # All temperatures are in Fahrenheit.
   DEFAULT_BASE = 50
   DEFAULT_UPPER = 86
   INVERSE_PI = 1 / Math::PI
 
+  def self.to_fahrenheit(celcius)
+    celcius.to_f * 9.0/5.0 + 32.0
+  end
+
+  def self.to_celcius(fahrenheit)
+    (fahrenheit.to_f - 32.0).to_f * 5.0/9.0
+  end
+
   def self.average_degree_days(min, max, base = DEFAULT_BASE)
-    degree_hours = ((max + min) / 2.0) - base
-    [degree_hours, 0.0].max
+    degree_days = ((max + min) / 2.0) - base
+    [degree_days, 0.0].max
   end
 
   def self.modified_degree_days(min, max, base = DEFAULT_BASE,
@@ -17,33 +25,37 @@ class DegreeDaysCalculator
     min = upper if upper < min
     max = upper if upper < max
 
-    average_degree_hours(min, max, base)
+    average_degree_days(min, max, base)
   end
 
   def self.sine_degree_days(min, max, base = DEFAULT_BASE,
                             upper = DEFAULT_UPPER)
+
     average = (min + max) / 2.0
+    return upper - base if (min >= upper) # both min and max greater than upper
+    return 0 if (max <= base) # both min and max less than base
+    # both min and max between base and upper
+    return average - base if (max <= upper && min >= base)
+
     alpha = (max - min) / 2.0
-    o1 = Math.asin((base - average) / alpha)
-    o2 = Math.asin((upper - average) / alpha)
-    
-    if (min >= upper) # both min and max are above the upper
-      return upper - base 
-    elsif (max > upper && min >= base) # max above upper, min between base/upper
-      return INVERSE_PI * ((average - base) * (o2 + INVERSE_PI) +
+    # max is between base and upper, min is less than base
+    if (max <= upper && min < base)
+      o1 = Math.asin((base - average) / alpha)
+      return INVERSE_PI * ((average - base) * (Math::PI / 2 - o1) +
+                           alpha * Math.cos(o1))
+    # max is greater than upper and min is between base and upper
+    elsif (max > upper && min >= base)
+      o2 = Math.asin((upper - average) / alpha)
+      return INVERSE_PI * ((average - base) * (o2 + Math::PI/2) +
                            (upper - base) * (Math::PI / 2 - o2) -
                            alpha * Math.cos(o2))
-    elsif (max > upper && min < base) # max above upper, min below base
+    # max is greater than upper and min is less than base
+    elsif (max > upper && min < base)
+      o1 = Math.asin((base - average) / alpha)
+      o2 = Math.asin((upper - average) / alpha)
       return INVERSE_PI * ((average - base) * (o2 - o1) +
                            alpha * (Math.cos(o1) - Math.cos(o2)) +
                            (upper - base) * (Math::PI / 2 - o2))
-    elsif (max <= upper && min >= base) # max and min both between base/upper 
-      return average - base 
-    elsif (max <= upper && min < base) # max between base/upper, min below base
-      return INVERSE_PI * ((average - base) * (Math::PI / 2 - o1) +
-                           alpha * Math.cos(o1))
-    else # min and max both below base
-      return 0
     end
   end
 end
