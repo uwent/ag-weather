64ff3b6a8531136a4e3588b1d3ecc916764ee40f
Allen Ballman <aballman@gmail.com>
2016-06-28 20:37:14 -0400

    Fix units in title of insolation map image.

--- app/controllers/insolations_controller.rb
+++ app/controllers/insolations_controller.rb
@@ -11,7 +11,7 @@ class InsolationsController < ApplicationController
       render json: { map: File.join(ImageCreator.url_path, 'no_data.png') }
     else
       insolations = Insolation.land_grid_values_for_date(date)
-      title = "Daily Insol (MJ day-1 m-1) for #{date.strftime('%-d %B %Y')}"
+      title = "Daily Insol (MJ day-1 m-2) for #{date.strftime('%-d %B %Y')}"
       image_name = ImageCreator.create_image(insolations, title,
                               "insolation_#{date.to_s(:number)}.png")
       render json: { map: File.join(ImageCreator.url_path, image_name) }
