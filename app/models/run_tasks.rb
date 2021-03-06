4fb5f677aa090494c1c122b8624a7d290f148995
Ben Bradford <bradfordium@gmail.com>
2021-03-06 12:53:06 -0600

    create single task script to coordinate daily tasks

---
+++ app/models/run_tasks.rb
@@ -0,0 +1,7 @@
+class RunTasks
+  WeatherImporter.fetch
+  InsolationImporter.fetch
+  EvapotranspirationImporter.create_et_data
+  PestForecastImporter.create_forecast_data
+  Evapotranspiration.create_and_static_link_image
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -2,24 +2,28 @@ set :output, "/tmp/whenever.log"
 set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
 job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
 
-every :day, at: '5am' do
-  runner "InsolationImporter.fetch"
-end
+# every :day, at: '5am' do
+#   runner "InsolationImporter.fetch"
+# end
 
-every :day, at: '5:45am' do # seems the earliest all the data for the day is there
-  runner "WeatherImporter.fetch"
-end
+# every :day, at: '5:45am' do # seems the earliest all the data for the day is there
+#   runner "WeatherImporter.fetch"
+# end
 
-every :day, at: '6:45am' do
-  runner "EvapotranspirationImporter.create_et_data"
-end
+# every :day, at: '6:45am' do
+#   runner "EvapotranspirationImporter.create_et_data"
+# end
 
-every :day, at: '6:46am' do
-  runner "PestForecastImporter.create_forecast_data"
-end
+# every :day, at: '6:46am' do
+#   runner "PestForecastImporter.create_forecast_data"
+# end
 
-every :day, at: '6:50am' do
-  runner "Evapotranspiration.create_and_static_link_image"
+# every :day, at: '6:50am' do
+#   runner "Evapotranspiration.create_and_static_link_image"
+# end
+
+every :day, at: '6:00am' do
+  runner "RunTasks"
 end
 
 every :day, at: '7:00am' do
