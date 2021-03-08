e1107b04ae26a26599e28aff11322d9cfd66c4e1
Ben Bradford <bradfordium@gmail.com>
2021-03-08 08:42:04 -0600

    update default value on migration

--- db/migrate/20210218202607_add_potato_p_days_to_pest_forecasts.rb
+++ db/migrate/20210218202607_add_potato_p_days_to_pest_forecasts.rb
@@ -1,5 +1,5 @@
 class AddPotatoPDaysToPestForecasts < ActiveRecord::Migration[6.1]
   def change
-    add_column :pest_forecasts, :potato_p_days, :float, :default => 0
+    add_column :pest_forecasts, :potato_p_days, :float, :default => 0.0
   end
 end
