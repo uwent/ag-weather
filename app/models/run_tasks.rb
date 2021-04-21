cb092edf1ec0951a5f5a350b4a1941394d77da0d
Ben Bradford <bradfordium@gmail.com>
2021-04-21 16:51:36 -0500

    Update run_tasks.rb

--- app/models/run_tasks.rb
+++ app/models/run_tasks.rb
@@ -1,44 +1,57 @@
 class RunTasks
 
+  # daily tasks to be run from scheduler
   def self.all
+    # fetch weather data from NOAA server
     WeatherImporter.fetch
+
+    # fetch insolation data from SSEC server
     InsolationImporter.fetch
+
+    # generate ETs from WeatherDatum and Insolation databases
     EvapotranspirationImporter.create_et_data
+
+    # generate pest forecasts for VDIFN from WeatherDatum
     PestForecastImporter.create_forecast_data
+
+    # generate ET image
     Evapotranspiration.create_and_static_link_image
   end
 
+  ## Command-line tools ##
+  # reports if WeatherDatum exists for each day in year
   def self.check_weather(year)
-    ActiveRecord::Base.logger.level = 1
-    (Date.new(year, 01, 01)..[Date.new(year, 12, 31), Date.today].min).each do |date|
+    dates = Date.new(year, 01, 01)..[Date.new(year, 12, 31), Date.today].min
+    dates.each do |date|
       if WeatherDatum.where(date: date).exists?
         puts date.strftime + " - ready"
       else
         puts date.strftime + " - no data"
       end
     end
-    ActiveRecord::Base.logger.level = 0
   end
 
+  # re-generates pest forecasts for year from WeatherDatum
+  # can be run if new models are added
   def self.redo_forecasts(year)
-    ActiveRecord::Base.logger.level = 1
-    (Date.new(year, 1, 1)..[Date.new(year, 12, 31), Date.today].min).each do |date|
+    dates = Date.new(year, 1, 1)..[Date.new(year, 12, 31), Date.today].min
+    dates.each do |date|
       redo_forecast(date)
     end
-    ActiveRecord::Base.logger.level = 0
   end
 
+  # re-generates pest forecasts for specific date range
   def self.redo_forecasts_for_range(start_date, end_date)
-    ActiveRecord::Base.logger.level = 1
-    (Date.parse(start_date)..Date.parse(end_date)).each do |date|
+    dates = Date.parse(start_date)..Date.parse(end_date)
+    dates.each do |date|
       redo_forecast(date)
     end
-    ActiveRecord::Base.logger.level = 0
   end
 
+  # re-generates pest forecast for date
   def self.redo_forecast(date)
     if WeatherDatum.where(date: date).exists?
-      puts date.strftime + " - recalculating..."
+      puts date.strftime + " - ready - recalculating..."
 
       weather = WeatherDatum.land_grid_for_date(date)
       forecasts = []
@@ -59,7 +72,7 @@ class RunTasks
       PestForecastDataImport.where(readings_on: date).delete_all
       PestForecastDataImport.create_successful_load(date)
     else
-      puts date.strftime + " - no weather data"
+      puts date.strftime + " - no data"
     end
   end
 end
