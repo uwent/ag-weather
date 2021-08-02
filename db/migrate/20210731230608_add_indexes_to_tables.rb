8e933cf1b074178a3d011e8aaa80592c45741cd0
Ben Bradford <bradfordium@gmail.com>
2021-08-02 14:53:15 -0500

    Update 20210731230608_add_indexes_to_tables.rb

--- db/migrate/20210731230608_add_indexes_to_tables.rb
+++ db/migrate/20210731230608_add_indexes_to_tables.rb
@@ -1,18 +1,18 @@
 class AddIndexesToTables < ActiveRecord::Migration[6.1]
   def change
-    add_index :evapotranspirations, [:latitude, :longitude]
-    add_index :insolations, [:latitude, :longitude]
-    add_index :pest_forecasts, [:latitude, :longitude]
-    add_index :weather_data, [:latitude, :longitude]
+    add_index :evapotranspirations, [:latitude, :longitude], if_not_exists: true
+    add_index :insolations, [:latitude, :longitude], if_not_exists: true
+    add_index :pest_forecasts, [:latitude, :longitude], if_not_exists: true
+    add_index :weather_data, [:latitude, :longitude], if_not_exists: true
 
-    add_index :evapotranspirations, :date
-    add_index :insolations, :date
-    add_index :pest_forecasts, :date
-    add_index :weather_data, :date
+    add_index :evapotranspirations, :date, if_not_exists: true
+    add_index :insolations, :date, if_not_exists: true
+    add_index :pest_forecasts, :date, if_not_exists: true
+    add_index :weather_data, :date, if_not_exists: true
 
-    add_index :evapotranspirations, [:latitude, :longitude, :date], unique: true
-    add_index :insolations, [:latitude, :longitude, :date], unique: true
-    add_index :pest_forecasts, [:latitude, :longitude, :date], unique: true
-    add_index :weather_data, [:latitude, :longitude, :date], unique: true
+    add_index :evapotranspirations, [:latitude, :longitude, :date], unique: true, if_not_exists: true
+    add_index :insolations, [:latitude, :longitude, :date], unique: true, if_not_exists: true
+    add_index :pest_forecasts, [:latitude, :longitude, :date], unique: true, if_not_exists: true
+    add_index :weather_data, [:latitude, :longitude, :date], unique: true, if_not_exists: true
   end
 end
