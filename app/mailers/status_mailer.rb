9d501ffb03825dd0b0e617da38dd7c144e4199d7
Ben Bradford <bradfordium@gmail.com>
2022-02-03 15:24:11 -0600

    add data import and status mailer tests

--- app/mailers/status_mailer.rb
+++ app/mailers/status_mailer.rb
@@ -1,8 +1,25 @@
 class StatusMailer < ActionMailer::Base
-  default from: "agweather@cals.wisc.edu"
+
+  def default_email
+    "agweather@cals.wisc.edu"
+  end
+
+  def admin_emails
+    User.where(admin: true).pluck(:email)
+  rescue
+    []
+  end
+
+  def status_recipients
+    admin_emails << default_email
+  end
 
   def status_mail(statuses)
     @statuses = statuses
-    mail to: "agweather@cals.wisc.edu", subject: "AgWeather status"
+    mail(
+      to: status_recipients,
+      from: default_email,
+      subject: "AgWeather status"
+    )
   end
 end
--- spec/models/data_import_spec.rb
+++ spec/models/data_import_spec.rb
@@ -31,58 +31,116 @@ RSpec.describe DataImport, type: :model do
         expect(DataImport.days_to_load).to match_array(dates_to_load - [reading_on])
       end
     end
+  end
 
-    describe ".on" do
-      it "returns DataImport records on date" do
-        expect(DataImport.on(Date.current)).to eq(DataImport.where(readings_on: Date.current))
+  describe ".on" do
+    it "returns DataImport records on date" do
+      expect(DataImport.on(Date.current)).to eq(DataImport.where(readings_on: Date.current))
+    end
+  end
+
+  describe ".start" do
+    it "creates a new DataImport record" do
+      expect { DataImport.start(Date.current) }.to change(DataImport, :count).by 1
+    end
+
+    it "or updates existing record" do
+      DataImport.start(Date.current)
+      expect { DataImport.start(Date.current) }.to change(DataImport, :count).by 0
+    end
+
+    it "creates a record with status started" do
+      new_record = DataImport.start(Date.current)
+      expect(new_record.status).to eq("started")
+    end
+  end
+
+  describe ".succeed" do
+    it "creates a new DataImport record" do
+      expect { DataImport.succeed(Date.current) }.to change(DataImport, :count).by 1
+    end
+
+    it "or updates existing record" do
+      DataImport.start(Date.current)
+      expect { DataImport.succeed(Date.current) }.to change(DataImport, :count).by 0
+    end
+
+    it "creates a record with status successful" do
+      record = DataImport.succeed(Date.current)
+      expect(record.status).to eq("successful")
+    end
+  end
+
+  describe ".fail" do
+    it "creates a new DataImport record" do
+      expect { DataImport.fail(Date.current) }.to change(DataImport, :count).by 1
+    end
+
+    it "or updates an existing record" do
+      DataImport.fail(Date.current)
+      expect { DataImport.fail(Date.current) }.to change(DataImport, :count).by 0
+    end
+
+    it "creates a record with status unsuccessful" do
+      record = DataImport.fail(Date.current)
+      expect(record.status).to eq("unsuccessful")
+    end
+  end
+
+  describe ".check_statuses" do
+    let(:day1) { Date.current - 2.days }
+    let(:day2) { Date.current - 1.day }
+    let(:statuses) { DataImport.check_statuses(day1, day2) }
+
+    describe "when all statuses successful" do
+      before(:each) do
+        FactoryBot.create(:weather_data_import, readings_on: day1, status: "successful")
+        FactoryBot.create(:weather_data_import, readings_on: day2, status: "successful")
+      end
+
+      it "checks all statuses in date range and reports none unsuccessful" do
+        expect(statuses).to be_a(Hash)
+        expect(statuses[:count]).to eq(0)
       end
     end
 
-    describe ".start" do
-      it "creates a new DataImport record" do
-        expect { DataImport.start(Date.current) }.to change(DataImport, :count).by 1
+    describe "when one status unsuccessful" do
+      before(:each) do
+        FactoryBot.create(:weather_data_import, readings_on: day1, status: "successful")
+        FactoryBot.create(:weather_data_import, readings_on: day2, status: "unsuccessful")
       end
 
-      it "or updates existing record" do
-        DataImport.start(Date.current)
-        expect { DataImport.start(Date.current) }.to change(DataImport, :count).by 0
+      it "checks all statuses in date range and reports one unsuccessful" do
+        expect(statuses).to be_a(Hash)
+        expect(statuses[:count]).to eq(1)
+      end
+    end
+  end
+
+  describe ".send_status_email" do
+    describe "when all statuses successful" do
+      before(:each) do
+        DataImport.earliest_date.upto(DataImport.latest_date) do |date|
+          FactoryBot.create(:weather_data_import, readings_on: date, status: "successful")
+        end
       end
 
-      it "creates a record with status started" do
-        new_record = DataImport.start(Date.current)
-        expect(new_record.status).to eq("started")
+      it "doesn't send an email" do
+        expect(StatusMailer).to_not receive(:status_mail)
+        DataImport.send_status_email
       end
     end
 
-    describe ".succeed" do
-      it "creates a new DataImport record" do
-        expect { DataImport.succeed(Date.current) }.to change(DataImport, :count).by 1
+    describe "when one status unsuccessful" do
+      before(:each) do
+        DataImport.earliest_date.upto(DataImport.latest_date) do |date|
+          FactoryBot.create(:weather_data_import, readings_on: date, status: "unsuccessful")
+        end
       end
 
-      it "or updates existing record" do
-        DataImport.start(Date.current)
-        expect { DataImport.succeed(Date.current) }.to change(DataImport, :count).by 0
-      end
-
-      it "creates a record with status successful" do
-        record = DataImport.succeed(Date.current)
-        expect(record.status).to eq("successful")
-      end
-    end
-
-    describe ".fail" do
-      it "creates a new DataImport record" do
-        expect { DataImport.fail(Date.current) }.to change(DataImport, :count).by 1
-      end
-
-      it "or updates an existing record" do
-        DataImport.fail(Date.current)
-        expect { DataImport.fail(Date.current) }.to change(DataImport, :count).by 0
-      end
-
-      it "creates a reco1rd with status unsuccessful" do
-        record = DataImport.fail(Date.current)
-        expect(record.status).to eq("unsuccessful")
+      it "sends an email" do
+        expect(StatusMailer).to receive(:status_mail).and_call_original
+        DataImport.send_status_email
       end
     end
   end
