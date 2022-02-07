9c7da3b4ee417adf62462265c48b999e05271ec3
Ben Bradford <bradfordium@gmail.com>
2022-02-07 11:43:46 -0600

    split gnuplot and composite calls in ImageCreator

--- app/models/image_creator.rb
+++ app/models/image_creator.rb
@@ -16,7 +16,7 @@ class ImageCreator
     File.join(temp_dir, "#{SecureRandom.urlsafe_base64(8)}.#{suffix}")
   end
 
-  def self.min_max(min, max)
+  def self.get_min_max(min, max)
     min ||= 0
     max ||= min
     # puts "#{min}, #{max} (#{tick}/tick) ==>"
@@ -37,19 +37,31 @@ class ImageCreator
   def self.create_image(grid, title, image_name, subdir: "", min_value: nil, max_value: nil)
     return "no_data.png" if grid.empty?
 
+    # get params from data
     data_min = grid.min.round(3)
     data_max = grid.max.round(3)
-    auto_min, auto_max = min_max(grid.min, grid.max)
+    auto_min, auto_max = get_min_max(grid.min, grid.max)
     min = min_value || auto_min
     max = max_value || auto_max
     max += 1 if min == max
+
+    # echo params
     Rails.logger.debug "ImageCreator :: Gunplot data range: #{data_min} -> #{data_max} = #{data_max - data_min} (#{(data_max - data_min) / 10.0}/tick)"
     Rails.logger.debug "ImageCreator :: Gunplot auto range: #{auto_min} -> #{auto_max} = #{auto_max - auto_min} (#{(auto_max - auto_min) / 10.0}/tick)"
     Rails.logger.debug "ImageCreator :: Gunplot display range: #{min} -> #{max} = #{max - min} (#{(max - min) / 10.0}/tick)"
 
-    datafile_name = create_data_file(grid)
-    image_name = generate_image_file(datafile_name, image_name, subdir, title, min, max, grid.extents)
-    image_name
+    # generate images
+    begin
+      datafile_name = create_data_file(grid)
+      gnuplot_image = run_gnuplot(datafile_name, title, min, max, grid.extents)
+      image_name = run_composite(gnuplot_image, image_name, subdir)
+      Rails.logger.debug "ImageCreator :: Created image #{image_name}"
+      image_name
+    rescue => e
+      Rails.logger.error "ImageCreator :: Failed to create image '#{image_name}': #{e.message}"
+      "no_data.png"
+    end
+    # image_name = generate_image_file(datafile_name, image_name, subdir, title, min, max, grid.extents)
   end
 
   def self.create_data_file(grid)
@@ -66,37 +78,30 @@ class ImageCreator
     data_filename
   end
 
-  # TODO: Maybe split the gnuplot and imagemagick calls into separate functions.
-  def self.generate_image_file(datafile_name, image_name, subdir, title, min_value, max_value, extents)
+  def self.run_gnuplot(datafile_name, title, min, max, extents)
     temp_image = temp_filename("png")
+
+    gnuplot_cmd = "gnuplot -e \"plottitle='#{title}'; min_val=#{min}; max_val=#{max}; x_min=#{extents[0]}; x_max=#{extents[1]}; y_min=#{extents[2]}; y_max=#{extents[3]}; outfile='#{temp_image}'; infile='#{datafile_name}';\" lib/color_contour.gp"
+    Rails.logger.debug "ImageCreator >> gnuplot cmd: #{gnuplot_cmd}"
+    `#{gnuplot_cmd}`
+
+    FileUtils.rm_f(datafile_name)
+    raise StandardError.new("ImageCreator :: Gnuplot execution failed!") if $?.exitstatus != 0
+    temp_image
+  end
+
+  def self.run_composite(gnuplot_image, image_name, subdir)
     image_dir = File.join(file_dir, subdir)
     FileUtils.mkdir_p(image_dir)
-    image_path = File.join(image_dir, image_name)
+    out_file = File.join(image_dir, image_name)
+    overlay_image = image_name.include?("-wi.png") ? "lib/wi_overlay.png" : "lib/map_overlay.png"
 
-    overlay_image = "lib/map_overlay.png"
-    overlay_image = "lib/wi_overlay.png" if image_name.include? "-wi.png" # TODO this is dumb
-
-    # Gnuplot
-    gnuplot_cmd = "gnuplot -e \"plottitle='#{title}'; min_val=#{min_value}; max_val=#{max_value}; x_min=#{extents[0]}; x_max=#{extents[1]}; y_min=#{extents[2]}; y_max=#{extents[3]}; outfile='#{temp_image}'; infile='#{datafile_name}';\" lib/color_contour.gp"
-    Rails.logger.debug ">> gnuplot cmd: #{gnuplot_cmd}"
-    `#{gnuplot_cmd}`
-    FileUtils.rm_f(datafile_name)
-    raise StandardError.new("ImageCreator :: Gnuplot execution failed!") if $?.exitstatus == 1
-
-    # Image Magick
-    image_cmd = "composite '#{overlay_image}' '#{temp_image}' '#{image_path}'"
-    Rails.logger.debug ">> imagemagick cmd: #{image_cmd}"
+    image_cmd = "composite '#{overlay_image}' '#{gnuplot_image}' '#{out_file}'"
+    Rails.logger.debug "ImageCreator >> imagemagick cmd: #{image_cmd}"
     `#{image_cmd}`
-    FileUtils.rm_f(temp_image)
-    raise StandardError.new("ImageCreator :: ImageMagick execution failed!") if $?.exitstatus == 1
-    Rails.logger.debug "ImageCreator :: Created image #{image_path}"
-    
+
+    FileUtils.rm_f(gnuplot_image)
+    raise StandardError.new("ImageCreator :: ImageMagick execution failed!") if $?.exitstatus != 0
     image_name
-  rescue => e
-    FileUtils.rm_f([datafile_name, temp_image])
-    Rails.logger.error "ImageCreator :: Failed to create image: #{e.message}\n" +
-      "Gnuplot cmd: #{gnuplot_cmd}\n" +
-      "ImageMagick cmd: #{image_cmd}"
-    "no_data.png"
   end
 end
