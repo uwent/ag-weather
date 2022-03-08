eb26d9e5cef988dd0f94fa09faf7f59177c6f8a0
Ben Bradford <bradfordium@gmail.com>
2022-03-07 19:02:27 -0600

    Update 20220307230636_update_indexes.rb

--- db/migrate/20220307230636_update_indexes.rb
+++ db/migrate/20220307230636_update_indexes.rb
@@ -3,8 +3,8 @@ class UpdateIndexes < ActiveRecord::Migration[7.0]
     add_index :data_imports, :readings_on, if_not_exists: true
 
     [:evapotranspirations, :insolations, :pest_forecasts, :precips, :weather_data].each do |t|
-      remove_index t, :date
-      remove_index t, [:latitude, :longitude, :date], unique: true
+      remove_index t, :date, if_exists: true
+      remove_index t, [:latitude, :longitude, :date], unique: true, if_exists: true
       add_index t, [:date, :latitude, :longitude], unique: true, if_not_exists: true
       add_index t, :longitude, if_not_exists: true
     end
