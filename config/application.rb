3ebac66a78b31172bc19bd70d12005886ac2fec5
Ben Bradford <bradfordium@gmail.com>
2023-02-17 11:18:36 -0600

    add nice logger formatting

--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,40 @@ Bundler.require(*Rails.groups)
 
 module AgWeather
   class MessageFormatter < ActiveSupport::Logger::SimpleFormatter
+    PAL = {
+      black: "\u001b[30m",
+      red: "\u001b[31m",
+      green: "\u001b[32m",
+      yellow: "\u001b[33m",
+      blue: "\u001b[34m",
+      magenta: "\u001b[35m",
+      cyan: "\u001b[36m",
+      white: "\u001b[37m",
+      reset: "\u001b[0m"
+    }
+
+    SEV = {
+      debug: "DBG",
+      info: "NFO",
+      warn: "WRN",
+      error: "ERR",
+      fatal: "FTL",
+      unknown: "UNK"
+    }
+
+    CLR = {
+      debug: PAL[:green],
+      info: PAL[:white],
+      warn: PAL[:yellow],
+      error: PAL[:red],
+      fatal: PAL[:red]
+    }
+
     def call(severity, time, progname, msg)
-      level = sprintf("%-5s", severity.to_s)
-      time = time.strftime("%Y-%m-%d %H:%M:%S")
-      msg = msg&.truncate(200, omission: "...#{msg.last(100)}")
-      "#{level} [#{time}] #{msg}\n"
+      sev = severity.to_sym.downcase
+      time = time.strftime("%Y%m%d %H:%M:%S")
+      msg = msg&.truncate(500, omission: "...")
+      "#{time} #{CLR[sev]}[#{SEV[sev]}] #{msg}#{PAL[:reset]}\n"
     end
   end
 
