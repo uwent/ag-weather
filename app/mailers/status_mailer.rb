af7b1e28d4371f3af0f4a2ab79e0ffbe1fb81127
Ben Bradford <bradfordium@gmail.com>
2023-03-23 18:47:56 -0500

    clean up host name

--- app/mailers/status_mailer.rb
+++ app/mailers/status_mailer.rb
@@ -1,6 +1,10 @@
 require "tzinfo"
 
 class StatusMailer < ActionMailer::Base
+  def host_name
+    ENV["AG_WEATHER_HOST"]&.gsub("https://", "") || "unknown host"
+  end
+
   def default_email
     "agweather@cals.wisc.edu"
   end
@@ -14,7 +18,7 @@ class StatusMailer < ActionMailer::Base
     mail(
       to: status_recipients,
       from: default_email,
-      subject: "Data import problem on #{ENV["AG_WEATHER_HOST"] || "unknown host"}"
+      subject: "Data import problem on #{host_name}"
     )
   end
 end
