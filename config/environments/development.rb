6031b24a280be3df5bc121dcb46446c90ff5ce2b
Ben Bradford <bbradford@wisc.edu>
2024-12-16 17:33:07 -0600

    lint per standardrb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-    config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
+    config.public_file_server.headers = {"cache-control" => "public, max-age=#{2.days.to_i}"}
   else
     config.action_controller.perform_caching = false
   end
@@ -51,7 +51,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   # Use letter opener for mail deliveries
   config.action_mailer.delivery_method = :letter_opener
